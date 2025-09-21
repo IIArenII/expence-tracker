06257afb36198250215f54d2b8862c66cfd11528
Aren <aerenevli@gmail.com>
2025-09-21 14:48:58 +0300

    Add latest frontend changes

--- frontend/src/api/expenses.ts
+++ frontend/src/api/expenses.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/expenses";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/expenses`;
 
 // Get all expenses
 export const getExpenses = async () => {
